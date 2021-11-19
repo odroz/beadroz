1d601f78e0aa351849115e7e729f94ebbae65055
Olivier Droz <droz.olivier@gmail.com>
2021-11-19 16:12:49 +0100

    only 1 API call and display repertoire

--- assets/js/gapi.js
+++ assets/js/gapi.js
@@ -1,20 +1,19 @@
 
-const googleScript = "https://script.google.com/macros/s/AKfycbwYQkiPvyHpVaZ9ffF7mekI-W-EZufvUmJrXl3y6kDeDc2tA7aK-A9enGencR1Hv1aB/exec";
-  const getConcerts = function(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       var concerts = JSON.parse(xhttp.responseText);
+const googleScript = "https://script.google.com/macros/s/AKfycbxZJq3x1wpZCn0AB2Yym0OSfcL-hyZq_Lcf6CoybmDbgM1sakx04Y--IClPj_XaFPkv/exec";
+  const displayConcerts = function(concerts){
        var concertHTML = "";
+       var icons = ["bi bi-calendar-event","bi bi-calendar-range", "bi bi-calendar-week" ]
+
+
        for(i=0; i< concerts.length; i++){
+         var icon = icons[getRandomInt(icons.length)];
          concertHTML += "<div class='col-md-6 d-md-flex align-items-md-stretch'>"+
            "<div class='count-box'>"+
-             "<i class='bi bi-calendar3-event'></i>"+
+             "<i class='"+icon+"'></i>"+
 
              "<span>"+concerts[i].Date+"</span>"+
              "<p>";
-             if(concerts[i].googleMaps){
+            if(concerts[i].googleMaps){
               concertHTML+="<a href='"+concerts[i].googleMaps+"' target='_blank'><b class='bi bi-geo-alt'></b></a>";
             }else{
                concertHTML+="<b class='bi bi-geo-alt'></b>";
@@ -31,9 +30,64 @@ const googleScript = "https://script.google.com/macros/s/AKfycbwYQkiPvyHpVaZ9ffF
        console.log(concerts);
        document.getElementById("dateConcerts").innerHTML = concertHTML;
 
-     }
+
     };
-    xhttp.open("GET", googleScript, true);
-    xhttp.send();
-  }
-window.onload = getConcerts();
+const displayRepertoire = function(repertoire){
+  var repertoireHTML = '';
+  repertoire.forEach(function(rep, i){
+    var middle = Math.floor(repertoire.length/2);
+    if(i == 0){
+      repertoireHTML+='<div class="col-lg-6">';
+    }
+    repertoireHTML+='<h3 class="resume-title">'+rep.categorie+'</h3>';
+    rep.compositeurs.forEach(function(c){
+      repertoireHTML+='<div class="resume-item">'+
+      '<h4>'+c.compositeur+'</h4>'+
+      '<ul>';
+      c.oeuvres.forEach(function(oeuvre){
+        repertoireHTML+='<li>'+oeuvre.nom;
+        if(oeuvre.partie){
+          repertoireHTML+='<span>'+oeuvre.partie+'</span>';
+        }
+        repertoireHTML+='</li>'
+      });
+      repertoireHTML+='</ul>'+
+      '</div>'
+    });
+    if(i == middle){
+      repertoireHTML+='</div>'+
+      '<div class="col-lg-6">';
+    }
+    if(i == repertoire.length){
+      repertoireHTML+='</div>';
+    }
+
+  });
+ document.getElementById("repertoire").innerHTML = repertoireHTML;
+};
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const getData = function(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     var data = JSON.parse(xhttp.responseText);
+     data.forEach(function(sheetData, i){
+                switch(i){
+                    case 0: displayConcerts(data[0]);
+                      break;
+                    case 1: displayRepertoire(data[1]);
+                      break;
+                    }
+                  }
+              );
+   }
+ }
+   xhttp.open("GET", googleScript, true);
+   xhttp.send();
+}
+
+window.onload = getData();
